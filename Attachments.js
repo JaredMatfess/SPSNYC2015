d32c29ee4f849e339230a696133ee3de128c0fa5
Jared Matfess <jaredm@slalom.com>
2015-07-25 11:05:36 -0400

    Script for disabling fields on Attachments web part

---
+++ Attachments.js
@@ -0,0 +1,25 @@
+﻿<script src = 'https://sp.slalom.com/ourfirm/offices/Hartford/dte/pcdemos/siteassets/jquery.js'></script>
+<script type='text/javascript'>
+
+$(document).ready(function () {
+//Clear the value in case there's any metadata associated with the document
+//Example: downloaded from ProjectAttachments list and has already been associated to a project
+$("[ID*='Request_x0020_ID']").val('');
+var ppxAttachID = getParameterByName("ID");
+$("[ID*='Request_x0020_ID']").attr("disabled","disabled");
+$("[ID*='DocAssigned']").attr("disabled","disabled");
+//document.getElementById("s4-ribbonrow").style.display = "none"; 
+
+});
+
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+  results = regex.exec(parent.location.search);
+  var finalString = (results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")));
+  return finalString;
+}
+
+
+</script>
---
+++ TrafficCop.js
@@ -0,0 +1,48 @@
+﻿<script src = 'https://sp.slalom.com/ourfirm/offices/Hartford/dte/pcdemos/siteassets/jquery.js'></script>
+<script type='text/javascript'>
+
+	var spsNYCURL = getURLParameterByName("Source");
+    var spsNYCreferrer = spsNYCURL.substring(spsNYCURL.lastIndexOf('/') + 1, spsNYCURL.lastIndexOf('.'));
+    var spsNYCbaseURL = location.href;
+	var checkActor =  getURLParameterByName("Actor");
+	
+	if(!checkActor)
+		{
+		switch(spsNYCreferrer) {
+		
+		case ("Reviewers"):
+		window.location.href = spsNYCbaseURL + "&Actor=ReadOnly&DefaultView=Reviewers";
+		break;
+
+	}
+}	
+	$(document).ready(function () {
+		
+		var smdbMaterialID = getURLParameterByName("ID");
+		
+		$.ajax({
+	    	url: "https://sp.slalom.com/ourfirm/offices/Hartford/dte/pcdemos/_api/web/lists/getbytitle('WorkRequest')/items('"+smdbMaterialID+"')",
+	    	method: "GET",
+	    	headers: {"accept": "application/json;odata=verbose"},
+	    	success: function(xData, status){
+				
+				document.title = xData.d.Request_x0020_ID;
+				
+				if(xData.d.Review_x0020_Status.indexOf('Approved') >= 0){
+				$('.ms-addnew').hide()
+				}
+				
+			}
+		});
+		
+	});
+	
+function getURLParameterByName(name){
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+  results = regex.exec(location.search);
+  var finalString = (results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")));
+  return finalString;
+};
+	 
+</script>
